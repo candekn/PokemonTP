32035f6e04cd388c741221bee7444250bff30ca7
Ivan Karlen <Ikarlen@alumno.unlam.edu.ar>
2019-05-06 13:01:12 -0300

    Con Sesion

--- Administrador/PokemonAdmin.php
+++ Administrador/PokemonAdmin.php
@@ -1,3 +1,13 @@
+<?php
+session_start();
+
+$usuario = $_SESSION['username'];
+
+if(!isset($usuario))
+{
+	header("location: ../login.php");
+}
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -41,7 +51,7 @@ require_once("recursos/encabezadoAdmin.php");
 //capturamos el id de la imagen que pasamos por parametro 
 $cod=$_GET["parametro"]; 
 
-$conect = mysqli_connect("localhost","root","Cuc41515","pokemons");// 
+$conect = mysqli_connect("localhost","root",11021998,"pokemons");// 
  
 	$busqueda=mysqli_query($conect, "SELECT * FROM personajes WHERE id =$cod"); 
  
